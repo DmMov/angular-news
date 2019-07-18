b70e757da68a6d00e188da12535825670a24e13d
Dm Mov <movchanyukd@gmail.com>
2019-07-18 03:28:55 +0300

    not found page is done

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.sass']
 })
 export class NotFoundComponent implements OnInit {
+  code: number = 404;
+  error: string = `error (${this.code})`;
+  title: string = 'sorry, we could not find the page you were looking for';
 
   constructor() { }
 
